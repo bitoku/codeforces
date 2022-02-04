4f9a7f49ed16b2e949fe095a284dc716d4e50ec9
Ayato Tokubi <tokubi.ayato@gmail.com>
2022-02-04 23:17:57 +0000

    678a

---
+++ 678/678a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll n, k;
+    cin >> n >> k;
+    cout << (n / k + 1) * k << endl;
+}

a43ef312a86534b87d14355932710e6d73a03873
Ayato Tokubi <tokubi.ayato@gmail.com>
2022-07-11 18:22:45 +0100

    660d

--- 660/660d.cpp
+++ 660/660d.cpp
@@ -4,62 +4,30 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-class FenwickTree {
-public:
-    ll n;
-    vector<ll> a;
-    explicit FenwickTree(ll _n): n(_n), a(_n+1, 0) {}
-
-    void add(ll i, ll x) {
-        i++;
-        if (i == 0) return;
-        for (ll k = i; k <= n; k+=(k & -k)) {
-            a[k] += x;
-        }
-    }
-
-    ll sum(ll i) {
-        i++;
-        ll s = 0;
-        if (i == 0) return s;
-        for (ll k = i; k > 0; k -= (k & -k)) {
-            s += a[k];
-        }
-        return s;
-    }
-};
-
-
 int main() {
     ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     int n;
     cin >> n;
-    vector<tuple<int, int, int>> q(n);
-    FenwickTree ft(2 * n);
-    map<int, int> mp;
+    vector<pair<int, int>> v(n);
     for (int i = 0; i < n; ++i) {
-        int a, b;
-        cin >> a >> b;
-        q[i] = {a, b, i};
-        mp[a];
-        mp[b];
+        cin >> v[i].first >> v[i].second;
     }
-    int t = 0;
-    for (auto& [a, k]: mp) {
-        k = t;
-        t++;
-    }
-    sort(q.begin(), q.end(), greater());
-    vector<int> result(n);
+    map<pair<int, int>, int> mp;
     for (int i = 0; i < n; ++i) {
-        auto [a, b, j] = q[i];
-        result[j] = ft.sum(mp[b]);
-        ft.add(mp[b], 1);
+        for (int j = i+1; j < n; ++j) {
+            auto x = v[i].first - v[j].first;
+            auto y = v[i].second - v[j].second;
+            if (x < 0 || (x == 0 && y < 0)) {
+                x = -x;
+                y = -y;
+            }
+            mp[make_pair(x, y)]++;
+        }
     }
-
-    for (int i = 0; i < n; ++i) {
-        cout << result[i] << '\n';
+    ll result = 0;
+    for (auto [p, k]: mp) {
+        result += k * (k - 1) / 2;
     }
-    cout << endl;
+    cout << result / 2 << endl;
 }
